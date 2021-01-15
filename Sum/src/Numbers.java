f568b1ab0e71d6a934e74b67cbb05bf8721604de
Marthely237 <n.marthely@alustudent.com>
2021-01-15 10:51:37 +0200

    Class Example

---
+++ Sum/src/Numbers.java
@@ -0,0 +1,12 @@
+public class Numbers {
+
+	public static void main(String[] args) {
+		
+		int num1 = 15, num2 = 24, Sum;
+		Sum = num1 + num2;
+		
+		System.out.println ("The sum is: "+Sum);
+
+	}
+
+}
